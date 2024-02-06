7b6037d2ddc13163758b2a8ec3e9e9e7fc8b44bd
Abel Borit <abelboritguitton@gmail.com>
2024-02-06 11:34:52 -0500

    [FEAT] index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/* exportar todos los archivos de forma individual que van a ser permitidos usar en otro proyecto  */
+export { MyLabel } from "./components/MyLabel";
+export { Button } from "./stories/Button";
+export { Header } from "./stories/Header";
+export { Page } from "./stories/Page";
