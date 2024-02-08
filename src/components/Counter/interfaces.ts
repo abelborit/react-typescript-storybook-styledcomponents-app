c2ee16c95d1f0adb9426220525945f15b1e78487
Abel Borit <abelboritguitton@gmail.com>
2024-02-08 12:18:21 -0500

    [FEAT] add comments in interfaces.ts to show it in the component docs in Storybook

--- src/components/Counter/interfaces.ts
+++ src/components/Counter/interfaces.ts
@@ -1,4 +1,10 @@
 export interface CounterProps {
+  /**
+   * Initial value of counter
+   */
   counterInitialValue: number;
+  /**
+   * Value to increase counter
+   */
   increaseValueBy: number;
 }
